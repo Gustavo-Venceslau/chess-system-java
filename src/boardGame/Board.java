fc3a46491d1eb77071b9324a20340ab88b48c411
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-02-07 23:04:30 -0300

    BoardException and defensive programming

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -7,6 +7,9 @@ public class Board  {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns < 1){
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -21,6 +24,9 @@ public class Board  {
     }
 
     public Piece piece(int row, int column){
+        if(!positionExists(row, column)){
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
@@ -29,8 +35,26 @@ public class Board  {
     }
 
     public void placePeace(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new BoardException("There is already a piece on position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
+    }
+
 
 }
---
+++ src/boardGame/BoardException.java
@@ -0,0 +1,8 @@
+package boardGame;
+
+public class BoardException extends RuntimeException{
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
