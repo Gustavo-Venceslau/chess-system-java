5dc7e4bcfedb5e19148c2e930392a3160f30bb33
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-02-08 20:13:49 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,8 +1,10 @@
 package chess;
 
+import boardGame.BoardException;
+
 import java.io.Serial;
 
-public class ChessException extends RuntimeException{
+public class ChessException extends BoardException {
     @Serial
     private static final long serialVersionUID = 1L;
 
